17fef9ca1861ca79fa770e6de345f1fcaf166813
Mokshith-S <mokshithnaikputtur@gmail.com>
2023-10-23 17:23:59 +0530

    font color option

--- lib/fore_color.dart/color_module.dart
+++ lib/fore_color.dart/color_module.dart
@@ -13,69 +13,42 @@ class ColorModule extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
-      itemCount: (colors.length ~/ 2) - 1,
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 2,
+      ),
+      itemCount: colors.length,
       itemBuilder: (context, index) {
-        List<Widget> setsOfRows = [];
-        for (int interCounter = index * 2;
-            interCounter <= index * 2 + 1;
-            interCounter++) {
-          setsOfRows.add(
-            TextButton(
-              onPressed: () {
-                changeFontColor(colors[colors.keys.toList()[interCounter]]!);
-                Navigator.pop(context);
-              },
-              child: Row(
-                children: [
-                  if (index * 2 == interCounter)
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(3),
-                      ),
-                      child: Icon(
-                        Icons.square_rounded,
-                        size: 18,
-                        color: colors[colors.keys.toList()[interCounter]],
-                      ),
-                    ),
-                  if (index * 2 == interCounter)
-                    const SizedBox(
-                      width: 6,
-                    ),
-                  if (index * 2 == interCounter)
-                    Text(colors.keys.toList()[interCounter]),
-                  if (index * 2 + 1 == interCounter)
-                    Text(colors.keys.toList()[interCounter]),
-                  if (index * 2 + 1 == interCounter)
-                    const SizedBox(
-                      width: 6,
-                    ),
-                  if (index * 2 + 1 == interCounter)
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: Colors.black,
-                        borderRadius: BorderRadius.circular(3),
-                      ),
-                      child: Icon(
-                        Icons.square_rounded,
-                        size: 18,
-                        color: colors[colors.keys.toList()[interCounter]],
-                      ),
-                    ),
-                ],
-              ),
+        return Container(
+          alignment: Alignment.topLeft,
+          child: TextButton(
+            onPressed: () {
+              changeFontColor(colors[colors.keys.toList()[index]]!);
+              Navigator.pop(context);
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(3),
+                  ),
+                  child: Icon(
+                    Icons.square_rounded,
+                    size: 18,
+                    color: colors[colors.keys.toList()[index]],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(colors.keys.toList()[index]),
+              ],
             ),
-          );
-          if (index * 2 == interCounter) setsOfRows.add(const Spacer());
-        }
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: setsOfRows,
+          ),
         );
       },
     );
